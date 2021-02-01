11db6953a30a0bb093e6b3767be0d25ae16d9e94
Gamaliel <gabanto@sigcomt.com>
2021-02-01 16:56:48 -0500

    guardar token en el localstorae

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,21 @@ import { UsuarioModel } from 'src/app/models/usuario.model';
 export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel;
-  constructor() { }
+  constructor( private _auth: AuthService) { }
 
   ngOnInit() {
   }
 
   login(form: NgForm) {
+    
     if (form.invalid) { return; };
-    console.log("imprimir si el formulario es valido");
-    console.log(form);
+
+    this._auth.login( this.usuario ).subscribe(res => {
+      console.log(res);
+    }, (err => {
+      console.log(err.error.error);
+    })) 
+
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ export class AuthService {
 
   private url = "https://identitytoolkit.googleapis.com/v1/accounts:";
   private apikey = "AIzaSyBZyCPLqyFGrsdvnXkAindSuQH4vB9WPSA";
+  userToken: string;
+
   // crear nuevos usuarios
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
@@ -20,7 +23,9 @@ export class AuthService {
 
   constructor(
     private http : HttpClient
-  ) { } 
+  ) {
+    this.leerToken();
+   } 
 
 
   logout() {
@@ -28,6 +33,21 @@ export class AuthService {
   }
 
   login(usuario: UsuarioModel) {
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    }
+    return this.http.post(
+      `${this.url}signInWithPassword?key=${this.apikey}`, 
+      authData
+    ).pipe(
+      map( resp => {
+        
+        this.guardarToken(resp['idToken']);
+        return resp;
+
+      })
+    )
 
   }
 
@@ -42,8 +62,34 @@ export class AuthService {
     return this.http.post(
       `${this.url}signUp?key=${this.apikey}`, 
       authData
+    ).pipe(
+      map( resp => {
+        
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     )
   }
+
+
+  private guardarToken(idToken: string) {
+
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+  }
+
+  leerToken() {
+
+    if ( localStorage.getItem('token') ) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+    
+    return this.userToken;
+  }
+
 
 
 }
